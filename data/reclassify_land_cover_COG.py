73d66ab1a2fcb04db9a53cfb00f513f5d491419f
Firza Riany <firzariany2@gmail.com>
2025-08-31 21:39:34 +0200

    Update logic in reclassifying land cover

--- data/reclassify_land_cover_COG.py
+++ data/reclassify_land_cover_COG.py
@@ -5,6 +5,8 @@ import dask
 import pandas as pd
 import rioxarray
 from pathlib import Path
+import gc
+import psutil
 
 # Set up a pipeline to create a COG for one landcover type (Forest), for one year, and for one country
 select_country = ["MYS", "CRI", "NZL", "NOR", "IDN"]
@@ -32,80 +34,92 @@ landcover_dict = {
     ],  # Shrubland, sparse veg, bare sea, water
 }
 
+
+def print_memory():
+    process = psutil.Process()
+    print(f"Memory usage: {process.memory_info().rss / 1024 / 1024:.1f} MB")
+
+
+# Call it before and after processing
+print("Checking memory before reclassification: ", print_memory())
+
+# Phase 1: Reclasasification (only if master file of land reclassification does not exist)
+
 for iso in select_country:
-    print(f"[START] Reclassifying redundant land categories for {iso}...")
-    iso_df = xr.open_dataset(f"data/{iso}/{iso}_master_land_cover.nc", chunks={})[
-        "lccs_class"
-    ]
-    iso_df = iso_df.rio.write_crs("EPSG:4326")
+    master_reclass = Path(f"data/{iso}/{iso}_reclassified_forest.nc")
 
-    # Drop duplicated years in the time dimension
-    years = pd.Series(iso_df.time.values)
-    unique_mask = ~years.duplicated()
-    iso_df = iso_df.isel(time=unique_mask.values)
+    if not master_reclass.exists():
+        print(f"[START] Reclassifying forest for {iso}...")
+        iso_df = xr.open_dataset(
+            f"data/{iso}/{iso}_master_land_cover.nc",
+            chunks="auto",
+        )["lccs_class"]
+        iso_df = iso_df.rio.write_crs("EPSG:4326")
 
-    # Check if all output files exist
-    out_dir = Path(f"data/COG/{iso}")
-    out_dir.mkdir(parents=True, exist_ok=True)
+        # Drop duplicated years in the time dimension
+        years = pd.Series(iso_df.time.values)
+        unique_mask = ~years.duplicated()
+        iso_df = iso_df.isel(time=unique_mask.values)
 
-    all_exist = True
-    for lc_type in landcover_dict.keys():
-        for y in years:
-            out_path = out_dir / f"{iso}_{lc_type}_{y}.tiff"
-            if not out_path.exists():
-                all_exist = False
-                break
+        # # Create one variable for one land cover type -> take up memory
+        # reclassify = {}
+        # for count, (type, code) in enumerate(landcover_dict.items()):
+        #     print(count + 1, type, code)
 
-        if not all_exist:
-            break
+        #     # Reclassify the codes for each landcover type
+        #     binary_mask = xr.where(iso_df.isin(code), count + 1, np.nan).astype(
+        #         "float32"
+        #     )
+        #     binary_mask.name = type  # Set variable name
+        #     reclassify[type] = binary_mask
 
-    if all_exist:
-        print(f"All COG files for {iso} already exist. Skipping reclassification.")
-        continue
+        #     # Clear intermediate results
+        #     del binary_mask
+        #     gc.collect()
 
-    # Create one variable for one land cover type
-    reclassify = {}
-    for count, (type, code) in enumerate(landcover_dict.items()):
-        print(count + 1, type, code)
+        # Create forest variable
+        binary_mask = xr.where(iso_df.isin(landcover_dict["Forest"]), 1, np.nan).astype(
+            "float32"
+        )
+        binary_mask.name = "Forest"
+        reclassify = {"Forest": binary_mask}
+        
+        # Clear intermediate results
+        del binary_mask
+        gc.collect()
 
-        # Reclassify the codes for each landcover type
-        binary_mask = xr.where(iso_df.isin(code), count + 1, np.nan).astype("float32")
-        binary_mask.name = type  # Set variable name
+        # A new xarray with time, lon, lat dimensions and one land type
+        landcover_reclass = xr.Dataset(reclassify)
 
-        reclassify[type] = binary_mask
+        # Add attributes to identify the data
+        land_class = []
+        for count, type in enumerate(landcover_dict.keys()):
+            land_class.append(f"{count+1}: {type}")
 
-    # A new xarray with time, lon, lat dimensions and five variables of land cover types
-    landcover_reclass = xr.Dataset(reclassify)
+        landcover_reclass.attrs["CRS"] = "EPSG:4326"
+        landcover_reclass.attrs["Country"] = iso
+        landcover_reclass.attrs["Land Classification"] = "Forest"
+        # landcover_reclass.attrs["Land Classification"] = ", ".join(land_class) -> for > 1 land cover
 
-    # Add attributes to identify the data
-    land_class = []
-    for count, type in enumerate(landcover_dict.keys()):
-        land_class.append(f"{count+1}: {type}")
+        # Export reclassified forest data
+        out_netcdf = Path(f"data/{iso}/{iso}_reclassified_forest.nc")
+        landcover_reclass.to_netcdf(
+            f"{out_netcdf}",
+            format="NETCDF4",
+            engine="netcdf4",
+        )
 
-    landcover_reclass.attrs["CRS"] = "EPSG:4326"
-    landcover_reclass.attrs["Country"] = iso
-    landcover_reclass.attrs["Land Classification"] = ", ".join(land_class)
+        print(
+            f"[END] Finished exporting reclassified forest data for {iso} to data/{iso}/{iso}"
+        )
 
-    print("[START] Exporting COG...")
+        # Clear memory after each country
+        del landcover_reclass, iso_df
+        gc.collect()
 
-    # Create 2D array: one year and one variable
-    year_list = landcover_reclass.time.values
-    for lc_type in ["Agriculture", "Forest"]:
-        print(lc_type)
+    else:
+        print(
+            f"Master reclassified forest data already exists for {iso}. Skipping reclassification."
+        )
 
-        # Export with 5-year timesteps instead of 1-year
-        # If I want to plot timeseries, I can load the master land cover data anyway
-        for i in range(0, len(year_list), 5):
-
-            landcover_2d = landcover_reclass.sel(time=year_list[i])[lc_type]
-            
-            # Set -9999 as nodata
-            landcover_2d = landcover_2d.rio.write_nodata(-9999)
-
-            # Export as COG
-            out_path = f"{out_dir}/{iso}_{lc_type}_{year_list[i]}.tiff"
-
-            landcover_2d.rio.to_raster(
-                out_path, driver="COG", compress="deflate", dtype="float32",
-            )
-        print(f"[END] Finished exporting a COG file for {lc_type}")
+print("Checking memory after reclassification: ", print_memory())
